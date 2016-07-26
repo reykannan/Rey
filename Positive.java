804ef33cfd69b9cee2426fb6ab42111d8f3b4ece
reykannan <reykannan@gmail.com>
2016-07-26 15:44:40 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.scanner;
+public class Positive
+{
+public static void main(String args[])
+{
+Scanner sc =new Scanner (System.in);
+int i = sc.nextInt();
+if(i>0)
+{
+System.out.println(53);
+}else if(i<0)
+{
+System.out.println(0);}
+else
+System.out.println(0);
+}
+}
