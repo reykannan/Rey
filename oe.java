6ba2748e7561afbd690bc9309424222b8d4c9ba0
reykannan <reykannan@gmail.com>
2016-07-26 15:54:09 +0530

    Create oe.java

---
+++ oe.java
@@ -0,0 +1,15 @@
+import java.util.scanner;
+public class oe
+{
+public static void main(String args[])
+{
+Scanner sc =new Scanner (System.in);
+int i = sc.nextInt();
+if(i%2==0)
+{
+System.out.println("EVEN");
+}else
+{System.out.println("ODD");
+}
+}
+}
