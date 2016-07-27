a092133b35c8ea95f2c68d22bb330ecd52353cb4
reykannan <reykannan@gmail.com>
2016-07-27 20:59:23 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Power{
+
+     public static void main(String []args){
+     Scanner sc =new Scanner(System.in);
+     int result =(int) Math.pow(2, 3);
+     System.out.println(result);
+     }
+}
